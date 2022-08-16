40c09cb23053ed49ab153ddc46c60c06f6c2bae7
daviazevdo <azvd.davi@gmail.com>
2022-08-16 14:17:33 -0300

    refatoração do componente Signs

--- src/components/Signs.jsx
+++ src/components/Signs.jsx
@@ -1,22 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../styles/numbers.css';
 
-class Signs extends Component {
-  render() {
-    const { value, className, handleClick } = this.props;
-    return (
-      <>
-        <button
-          onClick={handleClick}
-          className={`button-number ${value} ${className} signs`}
-          value={value}
-          id={value}
-        >
-          {value}
-        </button>
-      </>
-    );
-  }
+function Signs({ handleClick, value, className }) {
+  return (
+    <button
+      onClick={handleClick}
+      className={`button-number ${value} ${className} signs`}
+      value={value}
+      id={value}
+    >
+      {value}
+    </button>
+  );
 }
 
 export default Signs;
